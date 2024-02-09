88f8fff3f46f39e0b237dcb54e55a2de8f18f1e4
Firman Zain <firman@paydia.id>
2024-02-09 23:02:06 +0700

    adjustment search province & city from db or api

--- app/Http/Controllers/MasterCityController.php
+++ app/Http/Controllers/MasterCityController.php
@@ -5,43 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\MasterCities;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class MasterCityController extends Controller
 {
     
-    public function __construct()
-    {
+    public function __construct() {
 
     }
 
     public function searchCities(Request $request) {
+        $cities = [];
 
-        $cities = MasterCities::with('province')->get();
-
-        if (!empty($request->input())) {
-            $validator = Validator::make($request->all(), [
-                'id' => ['nullable', 'numeric'],
-            ],
-            [
-                'id.numeric' => 'error.invalid'
-            ]);
-     
-            if ($validator->fails()) {
-                $errors = $validator->errors()->toArray();
-                $firstError = reset($errors);
-                $errorsResponse = [];
-                foreach ($errors as $key => $value) {
-                    $errorsResponse[$key] = $value[0];
-                }
-                return response()->json([
-                    'message' => $firstError[0],
-                    'errors' => $errorsResponse,
-                ], 400);
-            }
+        if (empty($request->input())) {
+            $cities = $this->searchCitiesFromDb($request);
+        } else {
+            $this->validationRequest($request);
 
             if ($request->input('id')) {
-                $provinceId = $request->input('id');
-                $cities = MasterCities::with('province')->find($provinceId);
+                $cityId = $request->input('id');
+                $cities = $this->searchCitiesFromDb($request, $cityId);
                 if (!$cities) {
                     return response()->json([
                         'message' => 'error.not_found',
@@ -58,5 +42,106 @@ class MasterCityController extends Controller
             'data' => $cities
         ];
         return response()->json($response);
+    }
+
+    private function validationRequest(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'id' => ['nullable', 'numeric'],
+        ],
+        [
+            'id.numeric' => 'error.invalid'
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            $firstError = reset($errors);
+            $errorsResponse = [];
+            foreach ($errors as $key => $value) {
+                $errorsResponse[$key] = $value[0];
+            }
+            return response()->json([
+                'message' => $firstError[0],
+                'errors' => $errorsResponse,
+            ], 400);
+        }
+    }
+
+    public function searchCitiesFromDb(Request $request, $id = '') {
+        $searchFromDb = env('SEARCH_FROM_DB', true);
+        if (!$searchFromDb) {
+            return $this->searchFromThirdPartyApi($request, $id);
+        }
+
+        $cities = MasterCities::with('province')->get();
+        if ($id != '') {
+            $cities = MasterCities::with('province')->find($id);
+            if (!$cities) {
+                return false;
+            }
+        }
+        return $cities;
+    }
+
+    public function searchFromThirdPartyApi(Request $request, $id = '') {
+        $apiUrl = env('RAJAONGKIR_URL', 'https://api.rajaongkir.com/starter');
+        $apiKey = env('RAJAONGKIR_KEY', '0df6d5bf733214af6c6644eb8717c92c');
+
+        try {
+            $client = new Client();
+            $url = $apiUrl . '/city';
+            if ($id != '') {
+                $url .= '?id='.$id;
+            }
+            $response = $client->get($url, [
+                'headers' => [
+                    'key' => $apiKey,
+                    'Accept' => 'application/json',
+                ],
+            ]);
+            $data = json_decode($response->getBody(), true);
+            if (!isset($data['rajaongkir']['results'])) {
+                if ($id != '') {
+                    return false;
+                }
+                return [];
+            }
+
+            $data = $data['rajaongkir']['results'];
+            if ($id != '') {
+                $data = [
+                    "id" => $data["city_id"],
+                    "province_id" => $data["province_id"],
+                    "type" => $data["type"],
+                    "name" => $data["city_name"],
+                    "postal_code" => $data["postal_code"],
+                    "province" => [
+                        "id" => $data["province_id"],
+                        "name" => $data["province"]
+                    ]
+                    ];
+            } else {
+                $data = array_map(function ($result) {
+                    return [
+                        "id" => $result["city_id"],
+                        "province_id" => $result["province_id"],
+                        "type" => $result["type"],
+                        "name" => $result["city_name"],
+                        "postal_code" => $result["postal_code"],
+                        "province" => [
+                            "id" => $result["province_id"],
+                            "name" => $result["province"]
+                        ]
+                    ];
+                }, $data);
+            }
+
+            return $data;
+
+        } catch (ClientException $e) {
+            if ($id != '') {
+                return false;
+            }
+            return [];
+        }
     }
 }
--- app/Http/Controllers/MasterProvinceController.php
+++ app/Http/Controllers/MasterProvinceController.php
@@ -5,43 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\MasterProvinces;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class MasterProvinceController extends Controller
 {
     
-    public function __construct()
-    {
+    public function __construct() {
 
     }
 
     public function searchProvinces(Request $request) {
+        $provinces = [];
 
-        $provinces = MasterProvinces::all();
-
-        if (!empty($request->input())) {
-            $validator = Validator::make($request->all(), [
-                'id' => ['nullable', 'numeric'],
-            ],
-            [
-                'id.numeric' => 'error.invalid'
-            ]);
-     
-            if ($validator->fails()) {
-                $errors = $validator->errors()->toArray();
-                $firstError = reset($errors);
-                $errorsResponse = [];
-                foreach ($errors as $key => $value) {
-                    $errorsResponse[$key] = $value[0];
-                }
-                return response()->json([
-                    'message' => $firstError[0],
-                    'errors' => $errorsResponse,
-                ], 400);
-            }
+        if (empty($request->input())) {
+            $provinces = $this->searchProvincesFromDb($request);
+        } else {
+            $this->validationRequest($request);
 
             if ($request->input('id')) {
                 $provinceId = $request->input('id');
-                $provinces = MasterProvinces::find($provinceId);
+                $provinces = $this->searchProvincesFromDb($request, $provinceId);
                 if (!$provinces) {
                     return response()->json([
                         'message' => 'error.not_found',
@@ -58,5 +42,79 @@ class MasterProvinceController extends Controller
             'data' => $provinces
         ];
         return response()->json($response);
+    }
+
+    private function validationRequest(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'id' => ['nullable', 'numeric'],
+        ],
+        [
+            'id.numeric' => 'error.invalid'
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            $firstError = reset($errors);
+            $errorsResponse = [];
+            foreach ($errors as $key => $value) {
+                $errorsResponse[$key] = $value[0];
+            }
+            return response()->json([
+                'message' => $firstError[0],
+                'errors' => $errorsResponse,
+            ], 400);
+        }
+    }
+
+    public function searchProvincesFromDb(Request $request, $id = '') {
+        $searchFromDb = env('SEARCH_FROM_DB', true);
+        if (!$searchFromDb) {
+            return $this->searchFromThirdPartyApi($request, $id);
+        }
+
+        $provinces = MasterProvinces::all();
+        if ($id != '') {
+            $provinces = MasterProvinces::find($id);
+            if (!$provinces) {
+                return false;
+            }
+        }
+        return $provinces;
+    }
+
+    public function searchFromThirdPartyApi(Request $request, $id = '') {
+        $apiUrl = env('RAJAONGKIR_URL', 'https://api.rajaongkir.com/starter');
+        $apiKey = env('RAJAONGKIR_KEY', '0df6d5bf733214af6c6644eb8717c92c');
+
+        try {
+            $client = new Client();
+            $url = $apiUrl . '/province';
+            if ($id != '') {
+                $url .= '?id='.$id;
+            }
+            $response = $client->get($url, [
+                'headers' => [
+                    'key' => $apiKey,
+                    'Accept' => 'application/json',
+                ],
+            ]);
+            $data = json_decode($response->getBody(), true);
+            if (!isset($data['rajaongkir']['results'])) {
+                if ($id != '') {
+                    return false;
+                }
+                return [];
+            }
+
+            $data = $data['rajaongkir']['results'];
+
+            return $data;
+
+        } catch (ClientException $e) {
+            if ($id != '') {
+                return false;
+            }
+            return [];
+        }
     }
 }
